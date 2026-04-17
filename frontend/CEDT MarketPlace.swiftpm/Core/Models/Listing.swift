227faaa45eadd6d7dd95bc7422e62148a2fda846
Phacharx <Phachara5501@gmail.com>
2026-04-18 00:17:06 +0700

    feat: add Codable & Hashable models for Listing and User

---
+++ frontend/CEDT MarketPlace.swiftpm/Core/Models/Listing.swift
@@ -0,0 +1,28 @@
+//
+//  Listing.swift
+//  CEDT MarketPlace
+//
+//  Created by Phachara Charoenkitkul on 18/4/2569 BE.
+//
+
+import Foundation
+
+enum ListingStatus: String, Codable, Hashable {
+    case available = "AVAILABLE"
+    case reserved = "RESERVED"
+    case sold = "SOLD"
+}
+
+struct Listing: Codable, Identifiable, Hashable {
+    let id: String
+    let sellerId: String
+    let title: String
+    let price: Double
+    let isFree: Bool
+    let status: ListingStatus
+    let courseCode: String?
+    let images: [String]
+    
+    let contactLine: String?
+    let contactIG: String?
+}
---
+++ frontend/CEDT MarketPlace.swiftpm/Core/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  CEDT MarketPlace
+//
+//  Created by Phachara Charoenkitkul on 18/4/2569 BE.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable, Hashable {
+    let id: String
+    let studentId: String
+    let email: String
+    let displayName: String
+    let avatarUrl: String?
+}
